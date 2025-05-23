d55ba55ed21b28adc1d991d2a4bf5ae6cbf1e63f
Alif Emanuel <70676382+alifemanuel@users.noreply.github.com>
2025-05-23 17:22:03 +0000

    Aula 23/05/2025

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,23 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText";
 
 export default function App() {
+  const funcao = [
+    { title: "Meu título 1", text: "Meu texto 1" },
+    { title: "Meu título 2", text: "Meu texto 2" },
+    { title: "Meu título 3", text: "Meu texto 3" },
+    { title: "Meu título 4", text: "Meu texto 4" },
+    { title: "Meu título 5", text: "Meu texto 5" },
+    { title: "Meu título 6", text: "Meu texto 6" },
+    { title: "Meu título 7", text: "Meu texto 7" },
+    { title: "Meu título 8", text: "Meu texto 8" },
+    { title: "Meu título 9", text: "Meu texto 9" },
+    { title: "Meu título 10", text: "Meu texto 10" },
+  ];
   return (
-    <MyText />,
-    <MyText />,
-    <MyText />
+    <>
+      {funcao.map((item, index) => (
+        <MyText key= {index} title={item.title} text={item.text}></MyText>
+      ))}
+    </>
   );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,16 +1,11 @@
 import styles from "./MyText.module.css";
 
-export function MyText() {
+export function MyText({title, text}) {
   return (
     <div className={styles.container}>
       <div className={styles.div}>
-        <h1 className={styles.title}>Meu primeiro React App</h1>
-        <p className={styles.text}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
-          ipsum accusantium ex numquam deleniti, animi itaque soluta vitae
-          voluptatem illum, error totam veritatis explicabo nisi possimus harum
-          sed dolore. Quidem.
-        </p>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.text}>{text}</p>
       </div>
     </div>
   );
